53ba4acc8f9b3741c65c91558c8ff4a90c26cdf6
roshith <roshithprakash7@gmail.com>
2024-08-27 19:12:53 +0530

    Changed favicon

--- algorithms/math/sieveOfEratothenes.js
+++ algorithms/math/sieveOfEratothenes.js
@@ -3,16 +3,24 @@
 // For every true value in array, set all multiples of that value as false
 
 const sieveOfEratothenes = (num) => {
+    // Fill entire array with true
     const arr = new Array(num).fill(true)
 
     let count = 0
+    // Since 0 & 1 are not prime, set them to false
     arr[0] = false
     arr[1] = false
 
+    // Loop through all numbers
     for (let i = 2; i < num; i++) {
+
+        // If number was not checked / is Prime
         if (arr[i]) {
+
+            // Increase count
             count++
 
+            // Set all multiples of number as false
             for (let j = 2 * i; j < num; j = j + i) {
                 arr[j] = false
             }
