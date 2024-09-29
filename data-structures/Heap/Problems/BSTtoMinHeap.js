9dda7ec417111dc95642b5805a12f26a394e300e
roshith <roshithprakash7@gmail.com>
2024-09-29 13:15:41 +0530

    BST to min heap with added condition

--- data-structures/Heap/Problems/BSTtoMinHeap.js
+++ data-structures/Heap/Problems/BSTtoMinHeap.js
@@ -1,4 +1,5 @@
 // Convert given BST to Min Heap 
+// Condition : All values in left subtree must be smaller than values in right subtree
 
 // Node to represent a singular element in the tree
 class Node {
@@ -9,6 +10,7 @@ class Node {
     }
 }
 
+// Inorder traversal pushes value inside array
 const inOrder = (head, arr) => {
     if (!head) {
         return
@@ -18,42 +20,57 @@ const inOrder = (head, arr) => {
     inOrder(head.right, arr)
 }
 
+// Also known as Breadth First Search
+const levelOrder = (head) => {
+    console.log("\n")
+
+    // Create a Queue
+    const queue = []
+
+    // Push root node into queue
+    queue.push(head)
+    // Push null whenever a level is completed (since root only has one element; add null)
+    queue.push(null)
+
+    // While queue is not empty
+    while (queue.length) {
+
+        // Get the node at the front of queue
+        let temp = queue[0]
+        // Dequeue node
+        queue.shift()
+
+        // If value at front was null, level was completed
+        if (temp == null) {
+            // Add new line to signify new level
+            console.log("\n")
+            // If queue still has elements, push null
+            if (queue.length) {
+                queue.push(null)
+            }
+        }
+        // If value at front is a node
+        else {
+            // Print node value
+            process.stdout.write(String(temp?.value) + " ")
+
+            // Add left node to queue
+            if (temp?.left) {
+                queue.push(temp?.left)
+            }
+
+            // Add right node to queue
+            if (temp?.right) {
+                queue.push(temp?.right)
+            }
+        }
+    }
+}
+
 // Class for Binary Search Tree
 class BST {
     constructor() {
         this.head = null
-    }
-
-    // To get the inorder predecessor of the node
-    // Inorder predecessor of node is the rightmost element of the left subtree
-    getInOrderPredecessor(node) {
-        node = node.left
-        while (node.right != null) {
-            node = node.right
-        }
-
-        return node
-    }
-
-    // Search for the value in BST
-    search(node, value) {
-        // If node is null, value is not present in Tree
-        if (node == null) {
-            console.log(`Node with value ${value} is not present.`)
-            return
-        }
-        // If node = value, required Node has been found
-        if (node.value == value) {
-            return node
-        }
-        // If value is smaller than node, search left subtree
-        else if (node.value > value) {
-            return this.search(node.left, value)
-        }
-        // If value is larger than node, search the right subtree
-        else {
-            return this.search(node.right, value)
-        }
     }
 
     // Add a new node inside the BST
@@ -93,144 +110,49 @@ class BST {
         }
     }
 
+    // Populate array with inorder values 
     inOrder(arr) {
         inOrder(this.head, arr)
     }
 
-    // Delete a node from the BST
-    // 3 Cases :
-    //      1. Node is leaf node - change to null and return
-    //      2. Node has one child - replace with child
-    //      3. Node has 2 children - replace with inorder predecessor & perform delete operation for predecessor node
-    deleteNode(node, value) {
+    // Print level order traversal of tree
+    levelOrder() {
+        levelOrder(this.head)
+    }
 
-        // If node is null, element is not present in array
-        if (node == null) {
-            console.log("Couldn't find element!")
-            return null
-        }
+    // Converts BST to min heap
+    BSTToMinHeap(root, arr, index) {
+        if (root == null) return;
 
-        // If node is a leaf node, delete the node (No other operations need to be performed)
-        if (node.value == value && node.left == null && node.right == null) {
-            node = null
-            return null
-        }
+        // Copy index value to root
+        root.value = arr[index.index];
 
-        // If left is null, Replace node with node.right (Since only one child is present, we can replace it with its immediate child)
-        if (node.value == value && node.left === null) {
-            return node.right;
-        }
+        // Increment index
+        index.index++
 
-        // If right is null, Replace node with node.left (Since only one child is present, we can replace it with its immediate child)
-        else if (node.value == value && node.right === null) {
-            return node.left;
-        }
+        // then recur on left subtree
+        this.BSTToMinHeap(root.left, arr, index);
 
-        // If value is smaller than current node, move to left subtree
-        if (node && value < node.value) {
-            node.left = this.deleteNode(node.left, value)
-            return node
-        }
-        // If value is greater than current node, move to right subtree
-        else if (node && value > node.value) {
-            node.right = this.deleteNode(node.right, value)
-            return node
-        }
-        // If node is found, but is not a leaf node
-        else {
-            // Get the in-order predecessor of the node
-            let predecessor = this.getInOrderPredecessor(node)
-            // Replace node's value with predecessor value
-            node.value = predecessor.value
-            // Perform operation to remove predecessor node which was copied
-            node.left = this.deleteNode(node.left, predecessor.value)
+        // now recur on right subtree
+        this.BSTToMinHeap(root.right, arr, index);
+    }
 
-            return node
-        }
+    // Wrapper function to convert BST to heap
+    convertToMinHeap(root) {
+        // To populate inorder values
+        let arr = []
+
+        // Populate array
+        this.inOrder(arr)
+
+        // BST to MIN HEAP conversion
+        this.BSTToMinHeap(root, arr, { index: 0 });
     }
 }
 
-class Heap {
-    // Heap uses 1 based indexing and thus we add a placeholder value for 0th index
-    constructor() {
-        this.arr = [undefined]
-    }
 
-    // Insert a new value in the heap
-    insert(value) {
-        // Push a new value at the end of the heap
-        this.arr.push(value)
-        // Get index of newly pushed value
-        let index = this.arr.length - 1
+// ====================================================================================================================================
 
-        while (index > 1) {
-            // Get parent of new node
-            let parent = Math.floor(index / 2)
-
-            // In case of min heap - parent must be smaller than child
-            // If condition is not met, swap parent and child 
-            if (this.arr[parent] > this.arr[index]) {
-                [this.arr[parent], this.arr[index]] = [this.arr[index], this.arr[parent]]
-                index = parent
-            }
-            // If parent is snaller, simply return from function
-            else {
-                return
-            }
-        }
-    }
-
-    // Deletes the root value in the heap
-    delete() {
-
-        // Nothing is present inside the heap
-        if (this.arr.length === 1) {
-            console.log("Nothing to delete!");
-            return;
-        }
-
-        // Replace root with the last element
-        this.arr[1] = this.arr.pop();
-
-        let i = 1;  // Start from root
-
-        // Bubble down to maintain the min heap property
-        while (true) {
-            let left = 2 * i;
-            let right = 2 * i + 1;
-
-            // Assume current node (parent) is the smallest
-            let smallest = i;
-
-            // Compare with left child
-            if (left < this.arr.length && this.arr[left] < this.arr[smallest]) {
-                smallest = left;
-            }
-
-            // Compare with right child
-            if (right < this.arr.length && this.arr[right] < this.arr[smallest]) {
-                smallest = right;
-            }
-
-            // If the current node (parent) is smaller than both children, the heap is valid
-            if (smallest === i) {
-                break;
-            }
-
-            // Swap with the smaller child
-            [this.arr[i], this.arr[smallest]] = [this.arr[smallest], this.arr[i]];
-
-            // Move down to the child index
-            i = smallest;
-        }
-    }
-
-    printHeap() {
-        for (let i = 1; i < this.arr.length; i++) {
-            console.log(this.arr[i])
-        }
-    }
-}
 
 // BST used in the example
 // 
@@ -239,6 +161,7 @@ class Heap {
 //      2     6
 //     / \   /  \
 //    1   3 5    7
+
 
 const bst = new BST();
 
@@ -250,15 +173,8 @@ bst.addNode(bst.head, 6);
 bst.addNode(bst.head, 5)
 bst.addNode(bst.head, 7);
 
-let arr = []
-bst.inOrder(arr)
+bst.levelOrder()
 
-console.log(arr)
+bst.convertToMinHeap(bst.head)
 
-const heap = new Heap()
-
-for (let x of arr) {
-    heap.insert(x)
-}
-
-heap.printHeap()
+bst.levelOrder()
